0321439694009450c07d2425afe1fc60a8aa3050
Noureddine Sidi Abed <noureddinesidiabed6k@gmail.com>
2023-07-06 00:05:05 -0400

    implemented hero layout in the about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,29 +1,47 @@
 import Wrapper from "@/components/Wrapper";
+import { BodyText, SubTitle, Title } from "@/components/Typography";
 
 export default function About() {
   return (
     <>
       {/* Hero Section */}
-      <Wrapper>
-        <section className="py-64">
-          <h1>Hero Section</h1>
-        </section>
-      </Wrapper>
-      
+      <section className="flex flex-col border-b-2">
+        <Wrapper>
+          <header className="pt-20 flex justify-center flex-col text-center">
+            <text className="text-6xl font-semibold mb-4 ">
+              {" "}
+              About our company{" "}
+            </text>
+            <div className="h-16 max-w-3xl m-auto text-center">
+              <BodyText>
+                Condimentum mauris sit cursus amet id non neque pharetra nulla
+                ornare sed facilisis senectus dapibus nibh ultrices eget
+                suscipit aliquet et nulla magna.
+              </BodyText>
+            </div>
+          </header>
+
+          {/* Header Image Container */}
+          <div className="bg-blue-300 h-[700px]"></div>
+        </Wrapper>
+      </section>
+
+      {/* Statistics Section */}
+
       {/* Equipment Section */}
       <Wrapper>
         <section className="py-64">
           <h1>Equipment Section</h1>
         </section>
       </Wrapper>
-      
+
       {/* Values Section */}
       <Wrapper>
         <section className="py-64">
           <h1>Values Section</h1>
         </section>
       </Wrapper>
-      
+
       {/* Team Section */}
       <Wrapper>
         <section className="py-64">
