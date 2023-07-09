82c94d28312cf179ebaeae55729e60744518279c
Carlos Flores <carlosfloresalex@gmail.com>
2023-07-08 23:50:51 -0400

    added layout and style for the review section of the services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,6 @@
+import { SubTitle } from "@/components/Typography";
 import Wrapper from "@/components/Wrapper";
+import ReviewCard from "@/widgets/ReviewCard";
 
 export default function Services() {
   return (
@@ -13,7 +15,30 @@ export default function Services() {
       {/* Review Section */}
       <Wrapper>
         <section className="py-64">
-          <h1>Review Section</h1>
+          <div className="flex flex-col gap-5 mb-5 items-center">
+            <ul className="flex gap-5">
+              <li className="h-8 w-8 bg-blue-500"></li>
+              <li className="h-8 w-8 bg-blue-500"></li>
+              <li className="h-8 w-8 bg-blue-500"></li>
+              <li className="h-8 w-8 bg-blue-500"></li>
+              <li className="h-8 w-8 bg-blue-500"></li>
+            </ul>
+            <div className="max-w-xl text-center">
+              <SubTitle>5 out of 5 stars on Google Reviews</SubTitle>
+            </div>
+          </div>
+          <div className="flex lg:flex-row flex-col gap-10">
+            <ReviewCard
+              quote="“Condimentum mauris sit cursus amet id non neque pharetra nulla ornare sed dolorol facilisis senectus dolor conse.”"
+              name="Micheal Jackson"
+              location="New York City"
+            />
+            <ReviewCard
+              quote="“Condimentum mauris sit cursus amet id non neque pharetra nulla ornare sed dolorol facilisis senectus dolor conse.”"
+              name="Emily Smith"
+              location="Miami"
+            />
+          </div>
         </section>
       </Wrapper>
     </>
