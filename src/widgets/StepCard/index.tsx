a3c2cc0804a1fb1b908e18e839f63d92f09a1eab
Carlos Flores <carlosfloresalex@gmail.com>
2023-07-21 21:59:39 -0400

    added responsive styling to home page sections

--- src/widgets/StepCard/index.tsx
+++ src/widgets/StepCard/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { BodyText } from "@/components/Typography";
+import Image from 'next/image';
+import { BodyText } from '@/components/Typography';
 
 interface IStepCard {
   imageSrc: string;
@@ -10,12 +10,12 @@ interface IStepCard {
 
 export default function StepCard(props: IStepCard) {
   return (
-    <div className="w-96 flex flex-col">
+    <div className="flex-col max-w-md lg:mx-0 mx-auto ">
       <div className="w-60 h-60 bg-blue-400 mx-auto rounded-full mb-5">
         <Image src={props.imageSrc} alt={props.alt} />
       </div>
       <div className="text-center">
-        <h1 className="text-2xl font-bold text-gray-700 mb-2">{props.title}</h1>
+        <h1 className="text-xl font-bold text-gray-700 mb-2">{props.title}</h1>
         <BodyText>{props.description}</BodyText>
       </div>
     </div>
