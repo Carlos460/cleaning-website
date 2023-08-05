074e481119a0b3ab4af238e46417a585c945dae9
FullStackJoshua <Joshualim5131@gmail.com>
2023-08-05 01:56:20 -0400

    Updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,25 @@
+import Navbar from "@/widgets/Navbar";
+import Footer from "@/widgets/Footer";
+import { Title, SubTitle } from "@/components/Typography";
+import Wrapper from "@/components/Wrapper";
+import "../app/globals.css";
+import Button from "@/components/Button";
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <>
-      <h1>404-Page Not Found</h1>
+      <Navbar />
+      <Wrapper>
+        <section className="py-60 flex flex-col gap-5">
+          <h1 className="text-8xl text-gray-800 font-bold">Oops!!!</h1>
+          <SubTitle>Sorry, this page dosn't exist...</SubTitle>
+          <Link href="/">
+            <Button> Go back to Homepage </Button>
+          </Link>
+        </section>
+      </Wrapper>
+      <Footer />
     </>
   );
 }
