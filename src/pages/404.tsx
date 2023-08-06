faf773c740dd8b4d87caf4edfdd9edd4cdfdd3f9
Noureddine Sidi Abed <noureddinesidiabed6k@gmail.com>
2023-08-06 14:10:38 -0400

    Added animation component to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,18 +5,22 @@ import Wrapper from "@/components/Wrapper";
 import "../app/globals.css";
 import Button from "@/components/Button";
 import Link from "next/link";
+import AnimatePresence from "@/components/AnimatePresence";
 
 export default function Custom404() {
   return (
     <>
       <Navbar />
+
       <Wrapper>
         <section className="py-60 flex flex-col gap-5">
-          <h1 className="text-8xl text-gray-800 font-bold">Oops!!!</h1>
-          <SubTitle>Sorry, this page dosn&apos;t exist...</SubTitle>
-          <Link href="/">
-            <Button> Go back to Homepage </Button>
-          </Link>
+          <AnimatePresence>
+            <h1 className="text-8xl text-gray-800 font-bold">Oops!!!</h1>
+            <SubTitle>Sorry, this page dosn&apos;t exist...</SubTitle>
+            <Link href="/">
+              <Button> Go back to Homepage </Button>
+            </Link>
+          </AnimatePresence>
         </section>
       </Wrapper>
       <Footer />
